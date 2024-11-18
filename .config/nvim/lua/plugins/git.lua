8e88c5076acb2dedfedfe7d41264c607bbf30ab6
haedarrfd <hroofid@gmail.com>
2024-11-18 14:47:34 +0700

    Update some changes

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -17,10 +17,30 @@ return {
 				},
 			})
 
-			vim.keymap.set("n", "<leader>gp", "<cmd>:Gitsigns preview_hunk<CR>", { silent = true, desc = "Git preview" })
-			vim.keymap.set("n", "<leader>gg", "<cmd>:Gitsigns toggle_current_line_blame<CR>", { silent = true, desc = "Git blame" })
-			vim.keymap.set("n", "<leader>ge", "<cmd>:Gitsigns setqflist<CR>", { silent = true, desc = "Set quick fix list" })
-			vim.keymap.set("n", "<leader>gl", "<cmd>:Gitsigns setloclist<CR>", { silent = true, desc = "Set location list" })
+			vim.keymap.set(
+				"n",
+				"<leader>gp",
+				"<cmd>:Gitsigns preview_hunk<CR>",
+				{ silent = true, desc = "Git preview" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>gg",
+				"<cmd>:Gitsigns toggle_current_line_blame<CR>",
+				{ silent = true, desc = "Git blame" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>ge",
+				"<cmd>:Gitsigns setqflist<CR>",
+				{ silent = true, desc = "Set quick fix list" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>gl",
+				"<cmd>:Gitsigns setloclist<CR>",
+				{ silent = true, desc = "Set location list" }
+			)
 		end,
 	},
 }
