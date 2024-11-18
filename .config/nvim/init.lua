b3808e96516043327757402d63e0f74cbcbcfc16
haedarrfd <hroofid@gmail.com>
2024-11-18 14:44:28 +0700

    Add supernova filepath and update plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,11 +3,11 @@ vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 
 vim.g.netrw_browse_split = 0
-vim.g.netrw_winsize = 25
 
 vim.g.markdown_fenced_languages = {
 	"ts=typescript",
 }
 
 require("config.lazy")
-require("options")
+require("supernova.options")
+require("supernova.keymaps")
