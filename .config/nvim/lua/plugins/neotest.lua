3561a8677a3d53b23be427a246674124c1d473a0
haedarrfd <hroofid@gmail.com>
2024-11-23 07:59:24 +0700

    Add neotest plugin

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,42 @@
+return {
+	"nvim-neotest/neotest",
+	enabled = true,
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"marilari88/neotest-vitest",
+		"olimorris/neotest-phpunit",
+		{
+			"fredrikaverpil/neotest-golang",
+			version = "*",
+			dependencies = {
+				"leoluz/nvim-dap-go",
+			},
+		},
+	},
+	config = function()
+		local neotest = require("neotest")
+
+		neotest.setup({
+			adapters = {
+				require("neotest-golang")({
+					dap_go_enabled = true,
+				}),
+				require("neotest-vitest"),
+				require("neotest-phpunit"),
+			},
+		})
+
+		vim.keymap.set("n", "<leader>tm", function()
+			neotest.run.run()
+		end, { desc = "Test Method" })
+		vim.keymap.set("n", "<leader>tf", function()
+			neotest.run.run(vim.fn.expand("%"))
+		end, { desc = "Test File" })
+		vim.keymap.set("n", "<leader>ts", function()
+			neotest.run.run({ suite = true })
+		end, { desc = "Test All" })
+	end,
+}
