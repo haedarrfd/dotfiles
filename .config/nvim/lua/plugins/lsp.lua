ced343124ff2f62d1129e1378f29134e96062af9
haedarrfd <hroofid@gmail.com>
2024-11-29 13:02:18 +0700

    Update some changes

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -259,8 +259,8 @@ return {
 		"L3MON4D3/LuaSnip",
 		enabled = true,
 		version = "v2.*",
-		run = "make install_jsregexp",
 		dependencies = { "rafamadriz/friendly-snippets" },
+		run = "make install_jsregexp",
 		config = function()
 			local ls = require("luasnip")
 			ls.filetype_extend("javascript", { "jsdoc" })
